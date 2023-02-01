5e74f0e60c2ce570b417434e5b7b8c2a4736365c
urielch <uriel.chemouni@gmail.com>
2023-02-01 15:47:55 +0200

    refactor

--- generateLibClang.ts
+++ generateLibClang.ts
@@ -1,16 +1,16 @@
 import {
-    dirname,
-    join,
-    fromFileUrl,
-  } from "https://deno.land/std@0.170.0/path/mod.ts";
-  import { FFIgenerator } from "./generator/FFIgenerator.ts";
+  dirname,
+  join,
+  fromFileUrl,
+} from "https://deno.land/std@0.170.0/path/mod.ts";
+import { FFIgenerator } from "./generator/FFIgenerator.ts";
 
-// const includeDirectory = '/home/uriel/libclang_deno/build/include/'
-const includeDirectory = join(dirname(fromFileUrl(import.meta.url)), "sample", "libClang");
-const destination = join(dirname(fromFileUrl(import.meta.url)), "generated");
+const __dirname = dirname(fromFileUrl(import.meta.url));
+const includeDirectory = join(__dirname, "sample", "libClang");
+const destination = join(__dirname, "generated", 'clang');
 const includePaths = [
-    "/usr/lib/clang/14.0.0/include/",
-    `${includeDirectory}`,
+  "/usr/lib/clang/14.0.0/include/",
+  `${includeDirectory}`,
 ];
 const libFile = '/usr/lib/llvm-14/lib/libclang.so.1'
 const generator = new FFIgenerator({ headerRoot: includeDirectory, libFile, destination, includePaths });
